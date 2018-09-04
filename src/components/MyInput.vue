19f0edc29f298eaa505ead354c7ff744aa84a58f
Danilo Freire <danilo.cf.sjc@gmail.com>
2018-09-03 23:43:14 -0300

    add my input

---
+++ src/components/MyInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <label 
+      v-if="label" 
+      :for="id"
+      class="my-input__label"
+      >
+      {{ label }}:
+    </label>
+    <input
+      :type="type"
+      :id="id"
+      class="my-input"
+      :value="value"
+      @input="$emit('change', $event.target.value)"
+      :required="!notRequired"
+      >
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'value',
+    event: 'change'
+  },
+  props: {
+    value: String,
+    type: {
+      type: String,
+      required: false,
+      default: 'text'
+    },
+    id: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: false
+    },
+    notRequired: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.my-input {
+  font-size: 1.2rem;
+  width: 100%;
+  padding: 8px;
+  margin: .2rem 0 1rem;
+  box-sizing: border-box;
+}
+
+.my-input__label {
+  font-size: 1.2rem;
+}
+</style>
