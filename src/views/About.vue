34ec235f5752edfcef3b585a42183595caa14ba8
Danilo Freire <danilo.cf.sjc@gmail.com>
2018-09-08 00:54:57 -0300

    add mask lib

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <input 
+      type="text" 
+      v-mask="'money'"
+      class="my-input"
+      >
+
+    <input 
+      type="text" 
+      v-mask="'999.999.999-99'"
+      class="my-input"
+      >
   </div>
 </template>
+
+<script>
+import AwesomeMask from 'awesome-mask'
+
+export default {
+  directives: {
+    'mask': AwesomeMask
+  },
+  data() {
+    return {
+      value: 0
+    }
+  }
+}
+</script>
