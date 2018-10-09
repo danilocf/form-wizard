cce0dde1ee45b5fc6f1f22506cdc536cb8c1e924
Danilo Freire <danilo.cf.sjc@gmail.com>
2018-10-09 12:31:51 -0300

    update arch, add server folder, add base mongo setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const MongoClient = require('mongodb').MongoClient
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let db
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  if (err) return console.log(err)
+
+  db = client.db('test')
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+})
+
+app.get('/', (req, res) => {
+  db.collection('form').find().toArray(function (err, result) {
+    if (err) throw err
+    res.send(JSON.stringify(result))
+  })
+})
