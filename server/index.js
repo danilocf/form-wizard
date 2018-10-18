7d705a04be3492881e78bbfc441979b65e7101e1
Danilo Freire <danilo.cf.sjc@gmail.com>
2018-10-18 12:20:34 -0300

    setup mongo, get config, get users

--- server/index.js
+++ server/index.js
@@ -3,22 +3,26 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 const MongoClient = require('mongodb').MongoClient
+let db
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-let db
-
 MongoClient.connect('mongodb://localhost:27017', (err, client) => {
   if (err) return console.log(err)
-
-  db = client.db('test')
-
+  db = client.db('tg')
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 })
 
-app.get('/', (req, res) => {
-  db.collection('form').find().toArray(function (err, result) {
+app.get('/config', (req, res) => {
+  db.collection('form').find({}).toArray(function (err, result) {
     if (err) throw err
-    res.send(JSON.stringify(result))
+    res.send(result)
+  })
+})
+
+app.get('/users', (req, res) => {
+  db.collection('user').find({}).toArray(function (err, result) {
+    if (err) throw err
+    res.send(result)
   })
 })
