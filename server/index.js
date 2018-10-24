34b49b78bc85b34781d56ef1704391453d86f851
Danilo Freire <danilo.cf.sjc@gmail.com>
2018-10-24 09:59:38 -0300

    add cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 3000
 const MongoClient = require('mongodb').MongoClient
 const ObjectID = require('mongodb').ObjectID
 let db
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
