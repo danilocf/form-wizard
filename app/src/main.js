e020466e6eaaa8c6bdc500c9f84017ad5dcf8e23
Danilo Freire <danilo.cf.sjc@gmail.com>
2018-10-29 10:17:55 -0300

    add ptbr validator

--- app/src/main.js
+++ app/src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VeeValidate from 'vee-validate'
-import Vuelidate from 'vuelidate'
+import VeeValidate, { Validator } from 'vee-validate'
+import pt_BR from 'vee-validate/dist/locale/pt_BR'
 
-Vue.use(Vuelidate)
 Vue.use(VeeValidate)
+Validator.localize('pt_BR', pt_BR)
 Vue.config.productionTip = false
 
 new Vue({
